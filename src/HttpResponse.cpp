2d1d3b6a3502bbef8a06fd39b752e13f095f31ea
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-07-27 15:22:04 +0200

    Return error pages defined in server config

--- src/HttpResponse.cpp
+++ src/HttpResponse.cpp
@@ -1,40 +1,76 @@
 #include "HttpResponse.h"
 
+#include <fstream>
 #include <map>
+#include <sstream>
 #include <string>
 #include <utility>
 
 #include "Http.h"
 #include "HttpResponseStatus.h"
+#include "ServerConfig.h"
 
-HttpResponse::HttpResponse(HttpResponseInfo::code status_code)
-    : status_code_(HttpResponseInfo::status.at(status_code).first),
+HttpResponse::HttpResponse(HttpResponseInfo::code status_code, ServerConfig const& server_config)
+    : server_config_(server_config),
+      status_code_(HttpResponseInfo::status.at(status_code).first),
       reason_phrase_(HttpResponseInfo::status.at(status_code).second) {}
 
-HttpResponse::HttpResponse(HttpResponseSuccess::code status_code)
-    : status_code_(HttpResponseSuccess::status.at(status_code).first),
+HttpResponse::HttpResponse(HttpResponseSuccess::code status_code, ServerConfig const& server_config)
+    : server_config_(server_config),
+      status_code_(HttpResponseSuccess::status.at(status_code).first),
       reason_phrase_(HttpResponseSuccess::status.at(status_code).second) {}
 
 HttpResponse::HttpResponse(HttpResponseSuccess::code status_code, std::string const& body,
-                           std::string const& content_type)
-    : status_code_(HttpResponseSuccess::status.at(status_code).first),
+                           std::string const& content_type, ServerConfig const& server_config)
+    : server_config_(server_config),
+      status_code_(HttpResponseSuccess::status.at(status_code).first),
       reason_phrase_(HttpResponseSuccess::status.at(status_code).second),
       body_(body) {
   headers_["Content-Type"] = content_type;
 }
 
-HttpResponse::HttpResponse(HttpResponseRedir::code status_code)
-    : status_code_(HttpResponseRedir::status.at(status_code).first),
+HttpResponse::HttpResponse(HttpResponseRedir::code status_code, ServerConfig const& server_config)
+    : server_config_(server_config),
+      status_code_(HttpResponseRedir::status.at(status_code).first),
       reason_phrase_(HttpResponseRedir::status.at(status_code).second) {}
 
-HttpResponse::HttpResponse(HttpResponseClientError::code status_code)
-    : status_code_(HttpResponseClientError::status.at(status_code).first),
-      reason_phrase_(HttpResponseClientError::status.at(status_code).second) {}
+HttpResponse::HttpResponse(HttpResponseClientError::code status_code, ServerConfig const& server_config)
+    : server_config_(server_config),
+      status_code_(HttpResponseClientError::status.at(status_code).first),
+      reason_phrase_(HttpResponseClientError::status.at(status_code).second) {
+  std::map<enum HttpResponseClientError::code, std::string> client_error_pages = server_config.getclienterror();
+  std::map<enum HttpResponseClientError::code, std::string>::const_iterator status_page
+      = client_error_pages.find(status_code);
+  if (status_page != client_error_pages.end()) {
+    std::ifstream status_page_file(status_page->second);
+    if (status_page_file.is_open()) {
+      std::stringstream buffer;
+      buffer << status_page_file.rdbuf();
+      body_ = buffer.str();
+    } else {
+      std::cerr << status_page->second << " cannot be opened" << std::endl;
+    }
+  }
+}
 
-HttpResponse::HttpResponse(HttpResponseServerError::code status_code)
-    : status_code_(HttpResponseServerError::status.at(status_code).first),
-      reason_phrase_(HttpResponseServerError::status.at(status_code).second) {}
-
+HttpResponse::HttpResponse(HttpResponseServerError::code status_code, ServerConfig const& server_config)
+    : server_config_(server_config),
+      status_code_(HttpResponseServerError::status.at(status_code).first),
+      reason_phrase_(HttpResponseServerError::status.at(status_code).second) {
+  std::map<enum HttpResponseServerError::code, std::string> server_error_pages = server_config.getservererror();
+  std::map<enum HttpResponseServerError::code, std::string>::const_iterator status_page
+      = server_error_pages.find(status_code);
+  if (status_page != server_error_pages.end()) {
+    std::ifstream status_page_file(status_page->second);
+    if (status_page_file.is_open()) {
+      std::stringstream buffer;
+      buffer << status_page_file.rdbuf();
+      body_ = buffer.str();
+    } else {
+      std::cerr << status_page->second << " cannot be opened" << std::endl;
+    }
+  }
+}
 HttpResponse::~HttpResponse() {}
 
 std::string HttpResponse::getRaw() const {
--- src/HttpResponse.h
+++ src/HttpResponse.h
@@ -5,25 +5,28 @@
 #include <string>
 
 #include "HttpResponseStatus.h"
+#include "ServerConfig.h"
 
 class HttpResponse {
 public:
-  HttpResponse(HttpResponseInfo::code status_code);
-  HttpResponse(HttpResponseSuccess::code status_code);
-  HttpResponse(HttpResponseSuccess::code status_code, std::string const& body, std::string const& content_type);
-  HttpResponse(HttpResponseRedir::code status_code);
-  HttpResponse(HttpResponseClientError::code status_code);
-  HttpResponse(HttpResponseServerError::code status_code);
+  HttpResponse(HttpResponseInfo::code status_code, ServerConfig const& server_config);
+  HttpResponse(HttpResponseSuccess::code status_code, ServerConfig const& server_config);
+  HttpResponse(HttpResponseSuccess::code status_code, std::string const& body, std::string const& content_type,
+               ServerConfig const& server_config);
+  HttpResponse(HttpResponseRedir::code status_code, ServerConfig const& server_config);
+  HttpResponse(HttpResponseClientError::code status_code, ServerConfig const& server_config);
+  HttpResponse(HttpResponseServerError::code status_code, ServerConfig const& server_config);
 
   ~HttpResponse();
 
   std::string getRaw() const;
 
 private:
+  ServerConfig const server_config_;
   std::string const status_code_;
   std::string const reason_phrase_;
   std::string const content_type_;
-  std::string const body_;
+  std::string body_;
   std::map<std::string, std::string> headers_;
 };
 
