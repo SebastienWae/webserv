97c3b3e3dc1a21b148f9930331ceef7d194c9bb1
Jenny Eulliot <jeulliot@k2r3p6.42mulhouse.fr>
2022-07-29 15:36:39 +0200

    multiport handling - removed threads

--- src/main.cpp
+++ src/main.cpp
@@ -14,37 +14,18 @@
 
 int main(int argc, char** argv) {
   Config config;
-  std::list<pthread_t> threads;
-  std::list<Server> servers_list;
+  Server* s;
 
   try {
     config.checkconfig(Config::checkextension(argc, argv));
     config.parse();
 
-    std::vector<ServerConfig> const* servers = config.getServerConfig();
+    s = new Server(config);
+    s->run();
 
-    for (std::vector<ServerConfig>::const_iterator it = servers->begin(); it != servers->end(); ++it) {
-      Server* server = new Server(&(*it));
-      std::cout << std::endl
-                << "-----------> LAUNCHING ON PORT " << server->getConfig()->getport() << " <-----------" << std::endl
-                << std::endl;
-
-      servers_list.push_front(*server);
-      pthread_t th;
-
-      pthread_create(&th, NULL, &threadWrapper, server);
-      threads.push_front(th);
-    }
   } catch (std::exception& e) {
     std::cerr << e.what() << std::endl;
   }
-  for (std::list<pthread_t>::iterator it = threads.begin(); it != threads.end(); ++it) {
-    pthread_join(*it, 0);
-  }
-
-  for (std::list<Server>::iterator it = servers_list.begin(); it != servers_list.end(); ++it) {
-    delete &it;
-  }
-
+  delete s;
   return (0);
 }
