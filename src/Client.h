e2376f5e8e89c4e91505007f0886e3865f9e7b2b
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-08-02 10:11:05 +0200

    Add Client class

---
+++ src/Client.h
@@ -0,0 +1,43 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <ctime>
+#include <exception>
+
+#include "HttpRequest.h"
+
+class Client {
+public:
+  Client(int socket);
+
+  ~Client();
+
+  int getSocket() const;
+
+  void read(unsigned int bytes) throw(std::exception);
+  void send(unsigned int bytes) throw(std::exception);
+
+  HttpRequest* getRequest() const;
+
+  void setResponseData(std::string const& data);
+  void addResponseData(std::string const& data);
+  std::size_t getResponseSize() const;
+
+  bool isReading() const;
+  void setRead();
+
+  bool hasReplied() const;
+  void setReplied();
+
+  std::time_t const& getTime() const;
+
+private:
+  int socket_;
+  std::time_t timestamp_;
+  HttpRequest* request_;
+  std::string response_data_;
+  bool reading_;
+  bool replied_;
+};
+
+#endif
