b5019d787353e7a266c31e94ac882eab5f01c2de
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-08-02 10:10:48 +0200

    Refactor server

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,169 +1,310 @@
 #include "Server.h"
 
-#include <arpa/inet.h>
-#include <fcntl.h>
-#include <netinet/in.h>
-#include <sys/fcntl.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <vector>
 
-#include <algorithm>
-#include <exception>
-
+#include "Http.h"
 #include "HttpResponse.h"
 #include "HttpResponseStatus.h"
+#include "Log.h"
+#include "ServerConfig.h"
 
-Server::Server(ServerConfig const* config) : config_(config){};
+Server::Server(Config const& config) : config_(config), kq_(kqueue()) {}
 
-Server::~Server(){};
+Server::~Server() {
+  INFO("Stoping server");
 
-char const* Server::ServerCoreFatalException::what() const throw() { return "Server core fatal error."; }
-char const* Server::ServerCoreNonFatalException::what() const throw() { return "Server error."; }
-char const* Server::ListenerException::what() const throw() { return "Listener error."; }
-char const* Server::PollException::what() const throw() { return "Poll error."; }
-char const* Server::ClientGetRequestException::what() const throw() { return "Error while getting client request."; }
-char const* Server::ClientSendResponseException::what() const throw() {
-  return "Error while sending response to client.";
-}
-
-void* Server::getAddress(struct sockaddr* sockaddress) {
-  if (sockaddress->sa_family == AF_INET) {
-    return &(((struct sockaddr_in*)sockaddress)->sin_addr);
+  for (std::vector<int>::iterator it = ports_socket_.begin(); it != ports_socket_.end(); ++it) {
+    closeConnection(*it);
   }
-  return &(((struct sockaddr_in6*)sockaddress)->sin6_addr);
+
+  for (std::map<int, Client*>::iterator it = clients_.begin(); it != clients_.end(); ++it) {
+    closeConnection(it->first);
+    delete it->second;
+  }
+
+  close(kq_);
 }
 
-void Server::startListening() const {
-  if (listen(listener, MAX_PENDING_CONNECTIONS) < 0) {
-    std::cerr << "Listen failed" << std ::endl;
-    throw ListenerException();
+void Server::start() {  // NOLINT
+  INFO("Starting server");
+
+  std::set<std::string> ports = config_.getPorts();
+  for (std::set<std::string>::const_iterator it = ports.begin(); it != ports.end(); ++it) {
+    listenToPort(*it);
+  }
+
+  while (true) {
+    timespec timeout = {1, 0};
+    int n_events = kevent(kq_, NULL, 0, events_, KQ_SIZE, &timeout);  // NOLINT
+    if (n_events == 0) {
+      for (std::map<int, Client*>::iterator it = clients_.begin(); it != clients_.end(); ++it) {
+        if (it->second->getTime() + TIMEOUT <= std::time(nullptr)) {
+          timeoutClient(it->second);
+        }
+      }
+    } else if (n_events < 0) {
+      ERROR(std::strerror(errno));
+    }
+
+    for (int i = 0; i < n_events; ++i) {
+      int socket = static_cast<int>(events_[i].ident);
+
+      if (std::find(ports_socket_.begin(), ports_socket_.end(), socket) != ports_socket_.end()) {
+        acceptConnection(socket);
+      } else {
+        Client* client = findClient(socket);
+        try {
+          if (client == NULL) {
+            closeConnection(socket);
+            ERROR("Client not found");
+          } else if ((events_[i].flags & EV_EOF) != 0 || client->hasReplied()) {
+            removeClient(client);
+          } else if (events_[i].filter == EVFILT_READ) {
+            if (client->getTime() + TIMEOUT <= std::time(nullptr)) {
+              timeoutClient(client);
+            } else {
+              client->read(events_[i].data);
+              if (!client->isReading()) {
+                updateEvents(socket, EVFILT_READ, EV_DELETE);
+                updateEvents(socket, EVFILT_WRITE, EV_ADD | EV_ENABLE);
+                processRequest(client);
+              }
+            }
+          } else if (events_[i].filter == EVFILT_WRITE) {
+            if (!client->hasReplied()) {
+              client->send(events_[i].data);
+            } else {
+              removeClient(client);
+            }
+          }
+        } catch (std::exception& e) {
+          if (client == NULL) {
+            closeConnection(socket);
+          } else {
+            removeClient(client);
+          }
+        }
+      }
+    }
   }
 }
 
-void Server::createListenerSocket() {
-  int y = 1;
+void Server::processRequest(Client* client) {
+  HttpResponse* response;
+  ServerConfig const* server_config = config_.matchServerConfig(client->getRequest());
+
+  switch (client->getRequest()->getStatus()) {
+    case HttpRequest::S_NONE: {
+      response = new HttpResponse(HttpResponseServerError::_500, server_config);
+      client->setResponseData(response->getRaw());
+      delete response;
+      return;
+    }
+    case HttpRequest::S_OK: {
+      switch (client->getRequest()->getMethod()) {
+        case Http::UNKNOWN: {
+          response = new HttpResponse(HttpResponseClientError::_400, server_config);
+          client->setResponseData(response->getRaw());
+          delete response;
+          return;
+        }
+        case Http::GET:
+          getHandler(client, server_config);
+          return;
+        case Http::HEAD:
+          headHandler(client, server_config);
+          return;
+        case Http::POST:
+          postHandler(client, server_config);
+          return;
+        case Http::DELETE:
+          deleteHandler(client, server_config);
+          return;
+      }
+    }
+    case HttpRequest::S_CONTINUE: {  // this should not be possible
+      response = new HttpResponse(HttpResponseServerError::_500, server_config);
+      client->setResponseData(response->getRaw());
+      delete response;
+      return;
+    }
+    case HttpRequest::S_BAD_REQUEST: {
+      response = new HttpResponse(HttpResponseClientError::_400, server_config);
+      client->setResponseData(response->getRaw());
+      delete response;
+      return;
+    }
+    case HttpRequest::S_NOT_IMPLEMENTED: {
+      response = new HttpResponse(HttpResponseServerError::_501, server_config);
+      client->setResponseData(response->getRaw());
+      delete response;
+      return;
+    }
+    case HttpRequest::S_HTTP_VERSION_NOT_SUPPORTED: {
+      response = new HttpResponse(HttpResponseServerError::_505, server_config);
+      client->setResponseData(response->getRaw());
+      delete response;
+      return;
+    }
+    case HttpRequest::S_EXPECTATION_FAILED: {
+      response = new HttpResponse(HttpResponseClientError::_417, server_config);
+      client->setResponseData(response->getRaw());
+      delete response;
+      return;
+    }
+  }
+}
+
+void Server::getHandler(Client* client, ServerConfig const* server_config) {
+  Route const* route = server_config->matchRoute(client->getRequest()->getUri());
+
+  HttpResponse* response;
+  if (route->isAllowedMethod(Http::GET)) {
+    if (route->isRedirection()) {
+      response = new HttpResponse(route->getRedirection().first, route->getRedirection().second->getRaw());
+    } else {
+      response = new HttpResponse(HttpResponseSuccess::_200, server_config);
+    }
+  } else {
+    response = new HttpResponse(HttpResponseClientError::_405, server_config);
+  }
+
+  client->setResponseData(response->getRaw());
+  delete response;
+}
+
+// TODO
+void Server::headHandler(Client* client, ServerConfig const* server_config) {
+  HttpResponse response(HttpResponseSuccess::_200, server_config);
+  client->setResponseData(response.getRaw());
+}
+
+// TODO
+void Server::postHandler(Client* client, ServerConfig const* server_config) {
+  HttpResponse response(HttpResponseSuccess::_200, server_config);
+  client->setResponseData(response.getRaw());
+}
+
+// TODO
+void Server::deleteHandler(Client* client, ServerConfig const* server_config) {
+  HttpResponse response(HttpResponseSuccess::_200, server_config);
+  client->setResponseData(response.getRaw());
+}
+
+void Server::timeoutClient(Client* client) {
+  INFO("Timeout client");
+  if (client->isReading()) {
+    if (client->getRequest() != NULL) {
+      ServerConfig const* server_config = config_.matchServerConfig(client->getRequest());
+      HttpResponse timeout_response(HttpResponseClientError::_408, server_config);
+
+      client->setResponseData(timeout_response.getRaw());
+      client->setRead();
+    }
+
+    updateEvents(client->getSocket(), EVFILT_READ, EV_DELETE);
+    updateEvents(client->getSocket(), EVFILT_WRITE, EV_ADD | EV_ENABLE);
+  }
+}
+
+void Server::listenToPort(std::string const& port) {
+  INFO("Trying to listen to port: " + port);
+
+  struct addrinfo hints;
   memset((char*)&hints, 0, sizeof(hints));
-  hints.ai_family = AF_UNSPEC;
+  hints.ai_family = AF_INET;
   hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = AI_PASSIVE;
+  hints.ai_flags = 0;
 
-  if (getaddrinfo(NULL, (config_->getport()).c_str(), &hints, &address_info) != 0) {
-    std::cerr << "Getaddrinfo error." << std ::endl;
-    throw ListenerException();
+  struct addrinfo* address_info;
+  int error = getaddrinfo(NULL, port.c_str(), &hints, &address_info);
+  if (error != 0) {
+    ERROR(gai_strerror(error));
+    return;
   }
-  for (p = address_info; p != NULL; p = p->ai_next) {
-    listener = socket(p->ai_family, p->ai_socktype, p->ai_flags);
-    if (listener < 0) {
+  int port_socket;
+  struct addrinfo* tmp;
+  for (tmp = address_info; tmp != NULL; tmp = tmp->ai_next) {
+    port_socket = socket(tmp->ai_family, tmp->ai_socktype, tmp->ai_flags);
+    if (port_socket < 0) {
       continue;
     }
-    setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &y, sizeof(int));
-    if (fcntl(listener, F_SETFL, O_NONBLOCK) == -1) {
-      std::cerr << "Cannot set fd to non blocking." << std ::endl;
-      throw ListenerException();
+    int y = 1;
+    setsockopt(port_socket, SOL_SOCKET, SO_REUSEADDR, &y, sizeof(int));
+    if (fcntl(port_socket, F_SETFL, O_NONBLOCK) == -1) {
+      ERROR(std::strerror(errno));
+      return;
     }
-    if (bind(listener, p->ai_addr, p->ai_addrlen) < 0) {
-      close(listener);
+    if (bind(port_socket, tmp->ai_addr, tmp->ai_addrlen) < 0) {
+      close(port_socket);
+      ERROR(std::strerror(errno));
       continue;
     }
     break;
   }
   freeaddrinfo(address_info);
-  if (p == NULL) {
-    std::cerr << "Bind failed" << std ::endl;
-    throw ListenerException();
-  }
-};
-
-void Server::getClientRequest(int event_fd) {
-  address_len = sizeof(client_address);
-  new_socket = accept(event_fd, (struct sockaddr*)&client_address, (socklen_t*)&address_len);
-  if (new_socket == -1) {
-    perror("Accept socket error");
-  }
-  if (fcntl(new_socket, F_SETFL, O_NONBLOCK) == -1) {
-    std::cerr << "Cannot set new socket to non blocking." << std ::endl;
-    throw ListenerException();
-  }
-  EV_SET(change_event, new_socket, EVFILT_READ, EV_ADD, 0, 0, NULL);
-  if (kevent(kq, change_event, 1, NULL, 0, NULL) < 0) {
-    perror("kevent error");
-  }
-  std::cout << std::endl
-            << "*** New connection from "
-            << inet_ntop(client_address.ss_family, getAddress((struct sockaddr*)&client_address), remoteIP,
-                         INET6_ADDRSTRLEN)
-            << " ***" << std::endl
-            << std::endl;
-}
-
-void Server::sendingMessageBackToClient(int event_fd, HttpResponse const& response) {
-  char buffer[BUFSIZE_CLIENT_REQUEST];
-  // if (fcntl(event_fd, F_SETFL, O_NONBLOCK) == -1) {
-  //   std::cerr << "Cannot set new socket to non blocking." << std ::endl;
-  //   throw ListenerException();
-  // }
-  long int ret_recv = read(event_fd, buffer, BUFSIZE_CLIENT_REQUEST);
-
-  if (ret_recv == 0) {
-    std::cerr << std::endl << "Error: Connection closed by client" << std::endl;
+  if (tmp == NULL) {
+    ERROR("Socket creation error");
     return;
   }
-  if (ret_recv < 0) {
-    std::cerr << std::endl << "Error: No byte to read" << std::endl;
+  if (listen(port_socket, KQ_SIZE) < 0) {
+    ERROR(std::strerror(errno));
     return;
   }
-  buffer[ret_recv] = '\0';
-  std::cout << "Received from client : " << std ::endl << std::endl << buffer << std::endl;
-  std::string raw_response = response.getRaw();
-  std::cout << "Response sent to client : " << raw_response << std::endl << std::endl;
-  send(event_fd, (raw_response + "ok").c_str(), raw_response.length() + 2, 0);
-  std::cout << std::endl << "*** Message sent to client ***" << std::endl;
+  ports_socket_.push_back(port_socket);
+  updateEvents(port_socket, EVFILT_READ, EV_ADD | EV_ENABLE);
+  INFO("Listening to port: " + port);
 }
 
-void* Server::run() {
-  try {
-    createListenerSocket();
-    startListening();
-    kq = kqueue();
-    struct timespec timeout = {0, 0};  // pas de timeout => ne bloque pas
-    EV_SET(change_event, listener, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, 0);
-    if (kevent(kq, change_event, 1, NULL, 0, &timeout) == -1) {
-      perror("kevent");
-      throw ServerCoreFatalException();
-    }
-    while (true) {
-      try {
-        new_events = kevent(kq, NULL, 0, event, 1, &timeout);
-        if (new_events == -1) {
-          std::cout << "kevent error" << std::endl;
-          throw ServerCoreNonFatalException();
-        }
-        for (int i = 0; i < new_events; i++) {
-          int event_fd = event[i].ident;
-          if ((event[i].flags & EV_EOF) != 0) {
-            printf("Client has disconnected");
-            close(event_fd);
-          } else if (event_fd == listener) {
-            getClientRequest(event_fd);
-          } else if ((event[i].filter & EVFILT_READ) != 0) {
-            HttpResponse resp(HttpResponseSuccess::_200, "<html><body><h1>hello</h1></body></html>", "text/html",
-                              config_);
-            sendingMessageBackToClient(event_fd, resp);
-            close(event_fd);
-          }
-        }
-      } catch (ServerCoreNonFatalException& e) {
-        std::cerr << e.what() << std::endl;
-      }
-    }
-    close(listener);
-  } catch (ServerCoreFatalException& e) {
-    std::cerr << "FATAL ERROR - SERVER STOPPED LISTENING ON PORT " << config_->getport() << std::endl;
+void Server::closeConnection(int socket) {
+  INFO("Connection closed");
+
+  updateEvents(socket, EVFILT_READ, EV_DELETE);
+  updateEvents(socket, EVFILT_WRITE, EV_DELETE);
+
+  close(socket);
+}
+
+void Server::acceptConnection(int socket) {
+  INFO("New connection");
+
+  sockaddr_in client_addr;
+  int addr_len = sizeof(client_addr);
+  int connection_socket = accept(socket, (sockaddr*)&client_addr, (socklen_t*)&addr_len);
+  if (connection_socket == INVALID_SOCKET) {
+    return;
+  }
+
+  clients_[connection_socket] = new Client(connection_socket);
+
+  fcntl(connection_socket, F_SETFL, O_NONBLOCK);
+
+  updateEvents(connection_socket, EVFILT_READ, EV_ADD | EV_ENABLE);
+}
+
+Client* Server::findClient(int socket) {
+  std::map<int, Client*>::iterator client = clients_.find(socket);
+  if (client != clients_.end()) {
+    return client->second;
   }
   return NULL;
 }
 
-void* threadWrapper(void* current) { return (static_cast<Server*>(current))->run(); };
+void Server::removeClient(Client* client) {
+  int socket = client->getSocket();
+  closeConnection(socket);
+  delete client;
+  clients_.erase(socket);
+}
 
-ServerConfig const* Server::getConfig() const { return (config_); };
+void Server::updateEvents(int ident, short filter, u_short flags) {  // NOLINT
+  timespec timeout = {1, 0};
+  struct kevent kev;
+  EV_SET(&kev, ident, filter, flags, 0, 0, &timeout);
+  int n = kevent(kq_, &kev, 1, NULL, 0, 0);
+  if (n == 0) {
+    INFO("Kevent timeout");
+  } else if (n < 0) {
+    ERROR(std::strerror(errno));
+  }
+}
--- src/Server.h
+++ src/Server.h
@@ -3,86 +3,57 @@
 
 #include <netdb.h>
 #include <sys/event.h>
-#include <sys/time.h>
-#include <sys/types.h>
+#include <sys/fcntl.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
-#include <list>
+#include <algorithm>
+#include <exception>
+#include <iterator>
+#include <set>
+#include <vector>
 
-#include "HttpResponse.h"
+#include "Client.h"
+#include "Config.h"
 #include "ServerConfig.h"
 
-#define MAX_PENDING_CONNECTIONS 50   // TO SET
-#define BUFSIZE_CLIENT_REQUEST 4096  // TO SET
+#define KQ_SIZE 1024
+#define INVALID_SOCKET -1
+#define TIMEOUT 30000
 
-/* to use for multiple ports => multithread */
-void* threadWrapper(void* current);
 class Server {
 public:
-  Server(ServerConfig const* config);
+  Server(Config const& config);
   ~Server();
-  class ServerCoreFatalException : public std::exception {
-  public:
-    virtual char const* what() const throw();
-  };
 
-  class ServerCoreNonFatalException : public std::exception {
-  public:
-    virtual char const* what() const throw();
-  };
-
-  void* run();
-
-  /* Listener functions */
-  void createListenerSocket();
-  void startListening() const;
-  class ListenerException : public ServerCoreFatalException {
-  public:
-    virtual char const* what() const throw();
-  };
-
-  /* Poll functions */
-  void pollProcessInit();
-  class PollException : public ServerCoreNonFatalException {
-  public:
-    virtual char const* what() const throw();
-  };
-
-  /* Client functions */
-  static void* getAddress(struct sockaddr* sockaddress);
-  void getClientRequest(int event_fd);
-  class ClientGetRequestException : public ServerCoreNonFatalException {
-  public:
-    virtual char const* what() const throw();
-  };
-  void sendingMessageBackToClient(int event_fd, HttpResponse const& response);
-  class ClientSendResponseException : public ServerCoreNonFatalException {
-  public:
-    virtual char const* what() const throw();
-  };
-
-  ServerConfig const* getConfig() const;
-  /* TO DO : Setter and Getter to put these variables in private */
-  std::string port;
+  void start();
 
 private:
-  /*kqueue variables */
-  struct kevent change_event[4];
-  struct kevent event[4];
-  int new_events;
-  int kq;
+  Config const& config_;
 
-  /* Listener variables */
-  int listener;
-  struct addrinfo hints;
-  struct addrinfo* address_info;
-  struct addrinfo* p;
+  int const kq_;
+  struct kevent events_[KQ_SIZE];
 
-  /* Client request variables */
-  int new_socket;
-  sockaddr_storage client_address;
-  socklen_t address_len;
-  char remoteIP[INET6_ADDRSTRLEN];
-  ServerConfig const* config_;
+  std::vector<int> ports_socket_;
+
+  std::map<int, Client*> clients_;
+
+  void updateEvents(int ident, short filter, u_short flags);
+  void listenToPort(std::string const& port);
+  void closeConnection(int socket);
+  void acceptConnection(int socket);
+
+  Client* findClient(int socket);
+  void removeClient(Client* client);
+
+  void processRequest(Client* client);
+
+  void getHandler(Client* client, ServerConfig const* server_config);
+  void headHandler(Client* client, ServerConfig const* server_config);
+  void postHandler(Client* client, ServerConfig const* server_config);
+  void deleteHandler(Client* client, ServerConfig const* server_config);
+
+  void timeoutClient(Client* client);
 };
 
 #endif
