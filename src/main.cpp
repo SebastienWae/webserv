15e7774ac07daa43b284a96a0188b5ff21307c61
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-08-02 10:14:45 +0200

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,34 @@
-
-#include <pthread.h>
-
 #include <exception>
-#include <vector>
+#include <iostream>
 
 #include "Config.h"
 #include "Server.h"
-#include "ServerConfig.h"
 
-#if defined(__APPLE__) && defined(__MACH__)
-#  include <sys/_pthread/_pthread_t.h>
-#endif
+void run(Config const& config) {
+  try {
+    Server server(config);
+    server.start();
+  } catch (std::exception& e) {
+    std::cerr << "[CRITICAL ERROR]: restarting..." << std::endl;
+    run(config);
+  }
+}
 
 int main(int argc, char** argv) {
-  Config config;
-  std::list<pthread_t> threads;
-  std::list<Server> servers_list;
-
-  try {
-    config.checkconfig(Config::checkextension(argc, argv));
-    config.parse();
-
-    std::vector<ServerConfig> const* servers = config.getServerConfig();
-
-    for (std::vector<ServerConfig>::const_iterator it = servers->begin(); it != servers->end(); ++it) {
-      Server* server = new Server(&(*it));
-      std::cout << std::endl
-                << "-----------> LAUNCHING ON PORT " << server->getConfig()->getport() << " <-----------" << std::endl
-                << std::endl;
-
-      servers_list.push_front(*server);
-      pthread_t th;
-
-      pthread_create(&th, NULL, &threadWrapper, server);
-      threads.push_front(th);
+  if (argc == 1 || argc == 2) {
+    std::string config_path;
+    if (argc == 1) {
+      config_path = "./default.conf";
+    } else {
+      config_path = argv[1];
     }
-  } catch (std::exception& e) {
-    std::cerr << e.what() << std::endl;
+    // try {
+    Config config(config_path);
+    run(config);
+    // } catch (std::exception& e) {
+    // std::cerr << e.what() << std::endl;
+    // }
+  } else {
+    std::cout << "usage: " << argv[0] << " [/path/to/config/file.conf]" << std::endl;
   }
-  for (std::list<pthread_t>::iterator it = threads.begin(); it != threads.end(); ++it) {
-    pthread_join(*it, 0);
-  }
-
-  for (std::list<Server>::iterator it = servers_list.begin(); it != servers_list.end(); ++it) {
-    delete &it;
-  }
-
-  return (0);
 }
